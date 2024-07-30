bef80907afcba94680f28487e9b462e479e4b437
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-07-30 15:30:07 +0200

    bug fixes

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,6 +1,10 @@
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { Component } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -20,8 +24,14 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './dialog-add-player.component.html',
   styleUrl: './dialog-add-player.component.scss',
 })
-export class DialogAddPlayerComponent {
+export class DialogAddPlayerComponent implements OnInit {
   name: string = '';
 
-  constructor() {}
+  constructor(public dialogRef: MatDialogRef<DialogAddPlayerComponent>) {}
+
+  ngOnInit(): void {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -60,7 +60,9 @@ export class GameComponent implements OnInit {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
     dialogRef.afterClosed().subscribe((name: string) => {
-      this.game.players.push(name);
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
     });
   }
 }
